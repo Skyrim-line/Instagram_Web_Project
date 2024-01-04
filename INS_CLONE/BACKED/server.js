01621e31ec2e888e2f3ef241673c400c6f3a8f58
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-01-05 09:43:26 +1100

    update newest one

--- INS_CLONE/BACKED/server.js
+++ INS_CLONE/BACKED/server.js
@@ -1,3 +1,13 @@
 // create react app
 // npm install -g create-react-app
 // vite react app
+
+const express = require('express');
+// create a web server
+const app = express();
+const PORT = 8000;
+// 127.0.0.1
+app.listen(8000,()=>{
+    console.log('Server is running on http://localhost:8000')
+});
+
