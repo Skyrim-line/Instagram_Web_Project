6ffbf6e32c6bcfd88224074188cc3ab7bb729b7a
Simin Wu <76167009+Skyrim-line@users.noreply.github.com>
2024-03-11 18:02:35 +1100

    React lesson1

---
+++ workspace/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyClock from '../src/my-clock';
+
+function App() {
+
+  return (
+    <div className="App">
+      <MyClock></MyClock>
+    </div>
+  )
+
+}
+
+export default App;
---
+++ workspace/my-app/src/my-clock/index.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+function MyClock(props) {
+    const [time, setTime] = useState(new Date().toLocaleString());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleString());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        // class name 
+        <>
+            <h1>
+                React Clock
+            </h1>
+            <h2>{time}</h2>
+        </>
+
+    );
+}
+
+export default MyClock;
